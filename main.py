226b6ed6ff20e1a746a30bd39a357eb4ec423a91
zarak <ztariq@luc.edu>
2024-02-21 12:38:57 -0600

    load all datasets as authTokens

--- main.py
+++ main.py
@@ -11,16 +11,14 @@ from sodapy import Socrata
 
 domain = "data.cityofchicago.org"
 
-# attempt unauthenticated client
-client = Socrata(domain, None)
-crashes_results = client.get("85ca-t3if", limit=2000)
-
 # attempt an authenticated client
 clientAuth = Socrata(domain,
                      "YA4Qqo2jgEauLCdtGL8DRSJuk",
                      "zaraktariq7@gmail.com",
                      "xC|[F8H/bmL^^B@$^'E<s4e$C7_t%G/@_Wb2LB,<")
+crashes_results = clientAuth.get("85ca-t3if", limit=2000)
 vehicles_results = clientAuth.get("68nd-jvt3", limit=2000)
+people_results = clientAuth.get("u6pd-qa9d", limit=2000)
 
 # load datasets into dataframes
 crashes_master = pd.DataFrame.from_records(crashes_results)
@@ -29,10 +27,14 @@ working_crashes = crashes_master.copy(deep=True)
 vehicles_master = pd.DataFrame.from_records(vehicles_results)
 working_vehicles = vehicles_master.copy(deep=True)
 
+people_master = pd.DataFrame.from_records(people_results)
+working_people = people_master.copy(deep=True)
+
 if __name__ == '__main__':
     # print(working_crashes.head())
     # print(working_crashes.shape)
     # for column in range(working_crashes.shape[1]):
     #    print(working_crashes[working_crashes.columns[column]].count())
-    # print(working_crashes.describe())
+    print(working_crashes.describe())
     print(working_vehicles.describe())
+    print(working_people.describe())
